19fccf37858414d6dfd50e7ed8edad36187d60fb
surjeetg <surjeetg@opentext.com>
2026-07-01 12:16:52 +0530

    first commit

---
+++ application/src/main/java/com/example/application/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public  String hello() {
+        return "<h1>Hello, World!</h1>";
+    }
+}
+
